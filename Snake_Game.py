34b8044c4d2c960f3c4b75dc568265232ecf2031
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-06-06 15:28:23 +0530

    [MODIFIED]:Mustafa - Creating Screen for Snake

--- Snake_Game.py
+++ Snake_Game.py
@@ -4,3 +4,10 @@ import turtle
 import time
 import random
 
+#Creating a window screen 
+wnd = turtle.Screen()
+wnd.title("Snake Game by Mustafa-Kanchwala")
+wnd.bgcolor("RED")
+#Setting the Width and Height of Screen
+wnd.setup(width=750 , height=750)
+wnd.tracer(0)
