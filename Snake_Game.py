4ca4823c40af6a88b7f1b52622498e41d8e93083
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-06-08 11:04:25 +0530

    [MODIFIED]:Mustafa - UC-5-Complete Assigning keys to the game for snake to move

--- Snake_Game.py
+++ Snake_Game.py
@@ -7,9 +7,9 @@ import random
 #Creating a window screen 
 wnd = turtle.Screen()
 wnd.title("Snake Game by Mustafa-Kanchwala")
-wnd.bgcolor("RED")
+wnd.bgcolor("GREEN")
 #Setting the Width and Height of Screen
-wnd.setup(width=750 , height=750)
+wnd.setup(width=750 , height=650)
 wnd.tracer(0)
 
 #UC-2-Creating Mouth of the Snake  
@@ -22,7 +22,7 @@ mouth.direction = "Stop"
 
 #UC-3-Creating Food in Game
 food = turtle.Turtle()
-f_color = random.choice(["GREEN , BLUE , BLACK , ORANGE"])
+f_color = random.choice(["RED , BLUE , BLACK , ORANGE"])
 f_shape = random.choice(["SQUARE , CIRCLE , TRIANGLE"])
 food.speed(0)
 food.shape(f_shape)
@@ -39,3 +39,25 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0 , 250)
 pen.write("Score: 0  High score: 0",align="center",font=("Candara",24,"bold"))
+
+#UC-5-Assigning key Direction
+def go_up():
+    if mouth.direction !="down":
+        mouth.dirrection = "up"
+def go_down():
+    if mouth.direction != "up":
+        mouth.direction = "down"
+
+def go_right():
+    if mouth.direction != "left":
+        mouth.direction != "right"
+
+def go_left():
+    if mouth.direction != "right":
+        mouth.direction = "left"
+
+wnd.listen()
+wnd.onkeypress(go_up , "w")
+wnd.onkeypress(go_down ,"s")
+wnd.onkeypress(go_left , "a")
+wnd.onkeypress(go_right , "d")
